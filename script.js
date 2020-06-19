13b2b6dc053d130b5ecadc7061a65a21fd722985
youngMaster46 <kaplin95@inbox.ru>
2020-06-19 07:03:16 +0300

    add filtering posts

--- script.js
+++ script.js
@@ -46,6 +46,24 @@ function showLoading() {
     }, 1000)
 }
 
+// Filter posts by input
+function filterPosts(e) {
+    const term = e.target.value.toUpperCase()
+    const posts = document.querySelectorAll('.post')
+
+    posts.forEach(post => {
+        const title = post.querySelector('.post-title').innerText.toUpperCase()
+        const body = post.querySelector('.post-body').innerText.toUpperCase()
+
+        if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+            post.style.display = 'flex'
+        }
+        else {
+            post.style.display = 'none'
+        }
+    })
+}
+
 // Show initial posts
 showPosts();
 
@@ -60,3 +78,5 @@ window.addEventListener('scroll', () => {
         showLoading()
     }
 })
+
+filter.addEventListener('input', filterPosts)

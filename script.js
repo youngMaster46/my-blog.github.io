f00bc562549fe13d9566e25f6feea2c289540668
youngMaster46 <kaplin95@inbox.ru>
2020-06-19 06:30:32 +0300

    add posts in DOM

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const postsContainer = document.getElementById('posts-container')
+const postsContainer = document.getElementById('post-container')
 const loading = document.querySelector('.loader')
 const filter = document.getElementById('filter')
 
@@ -13,3 +13,24 @@ async function getPosts() {
 
     return data;
 }
+
+// Show posts in DOM
+async function showPosts() {
+    const posts = await getPosts();
+
+    posts.forEach(post => {
+        const postEl = document.createElement('div')
+        postEl.classList.add('post')
+        postEl.innerHTML = `
+        <div class="number">${post.id}</div>
+        <div class="post-info">
+            <h2 class="post-title">${post.title}</h2>
+            <p class="post-body">${post.body}</p>
+        </div>
+        `
+        postsContainer.appendChild(postEl)
+    })
+}
+
+// Show initial posts
+showPosts();
